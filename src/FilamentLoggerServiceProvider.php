ab2037a284a7a97e61cf20bc5fe3da7e08aa2377
Jord <jordan@baseserv.comGit>
2026-01-14 12:31:30 +0000

    Pushing to v4

--- src/FilamentLoggerServiceProvider.php
+++ src/FilamentLoggerServiceProvider.php
@@ -2,55 +2,35 @@
 
 namespace TomatoPHP\FilamentLogger;
 
-use Illuminate\Support\ServiceProvider;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 use TomatoPHP\FilamentLogger\EventServiceProvider;
 use TomatoPHP\FilamentLogger\Services\Benchmark;
 
-
-class FilamentLoggerServiceProvider extends ServiceProvider
+class FilamentLoggerServiceProvider extends PackageServiceProvider
 {
-    public function register(): void
+    public static string $name = 'filament-logger';
+
+    public function configurePackage(Package $package): void
     {
-        //Register generate command
-        $this->commands([
-           \TomatoPHP\FilamentLogger\Console\FilamentLoggerInstall::class,
-        ]);
+        $package
+            ->name(static::$name)
+            ->hasConfigFile('filament-logger')
+            ->hasMigrations()
+            ->hasTranslations()
+            ->hasCommand(\TomatoPHP\FilamentLogger\Console\FilamentLoggerInstall::class);
+    }
 
-        //Register Config file
-        $this->mergeConfigFrom(__DIR__.'/../config/filament-logger.php', 'filament-logger');
-
-        //Publish Config
-        $this->publishes([
-           __DIR__.'/../config/filament-logger.php' => config_path('filament-logger.php'),
-        ], 'filament-logger-config');
-
-        //Register Migrations
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-        //Publish Migrations
-        $this->publishes([
-           __DIR__.'/../database/migrations' => database_path('migrations'),
-        ], 'filament-logger-migrations');
-
-
-        //Register Langs
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'filament-logger');
-
-        //Publish Lang
-        $this->publishes([
-           __DIR__.'/../resources/lang' => base_path('lang/vendor/filament-logger'),
-        ], 'filament-logger-lang');
-
-
+    public function packageRegistered(): void
+    {
         Benchmark::start(config('filament-logger.request.benchmark', 'application'));
 
         $this->app->bind('filament-logger', function () {
             return new \TomatoPHP\FilamentLogger\Services\LoggerServices();
         });
-
     }
 
-    public function boot(): void
+    public function packageBooted(): void
     {
         $this->app->register(EventServiceProvider::class);
     }
